d71c9b746917a6bae57c98a76fe1b6a2e0adbae3
felipeeco <contacto@felipemoreno.co>
2021-01-22 15:28:08 -0500

    antes de hacer las actividades con tres columnas

--- pagina hecha con vie/codigo.js
+++ pagina hecha con vie/codigo.js
@@ -355,42 +355,21 @@ Vue.component('actividades', {
                             </div>
                             <div class="mt-3 txt">
                                 <strong>Tipo de programa</strong> <span>{{ item.tipoPrograma }}</span><br />
-                                <strong>Facultad</strong> <span v-html="programasMethod(item.facultad)"></span><br />
+                                <strong>Facultad</strong> <ul class="lista-facultad" v-html="programasMethod(item.facultad)"></ul><br />
                                 <strong>Programa</strong> 
-                                <span v-html="programasMethod(item.programa)"></span><br />
+                                <ul class="lista-programas" v-html="programasMethod(item.programa)"></ul><br />
                                 <strong>Categoria</strong> <span>{{ item.categoria }}</span>
                             </div>
                         </div>
                         <div class="col-md-8 col-lg-8 Descripcion">
                             <p class="titulo">{{ item.nombre}}</p>
                             <p>{{ item.contenido}}</p>
-                            <!--
-                            <p class="txt">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &#39;Content here, content here&#39;, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for &#39;lorem ipsum&#39; will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                            -->
-                            <!--
-                            <i class="fas fa-map-marker-alt">
-                                
-                            </i>
-                            -->
-                            <!--
-                            <strong>Lugar:</strong> <span class="txt">Universidad del Rosario, Sede, Claustro, Aula Mutis</span><br />
-                            <i class="far fa-calendar">
-                                
-                            </i>
-                            -->
                             <strong>Fecha de inicio:</strong> <span class="txt">
                             {{ dia(item.fechaInicio) }} {{ mes(item.fechaInicio) }}, de {{ year(item.fechaInicio) }}
                             </span><br />
                             <strong>Fecha de cierre:</strong> <span class="txt">
                             {{ dia(item.fechaFin) }} {{ mes(item.fechaFin) }}, de {{ year(item.fechaFin) }}
                             </span><br />
-                            <!--
-                            <i class="far fa-clock">
-                            
-                            </i>
-                    
-                            <strong>Hora:</strong> <span class="txt">De 8:00 a.m. a 12:30 p.m</span>
-                            -->
                             <div class="row">
                                 <div class="col-lg-3 Semestre_uno"><a href="">{{ item.periodo }}</a></div>
                                 <div class="col-lg-3 Cerrado"><a href="">{{ estado(item.fechaFin) }}</a></div>
@@ -419,41 +398,20 @@ Vue.component('actividades', {
                             </div>
                             <div class="mt-3 txt">
                                 <strong>Tipo de programa</strong> <span>{{ item.tipoPrograma }}</span><br />
-                                <strong>Facultad</strong> <span v-html="programasMethod(item.facultad)"></span><br />
+                                <strong>Facultad</strong> <ul class="lista-facultad" v-html="programasMethod(item.facultad)"></ul><br />
                                 <strong>Programa</strong> 
-                                <span v-html="programasMethod(item.programa)"></span><br />
+                                <ul class="lista-programas" v-html="programasMethod(item.programa)"></ul><br />
                                 <strong>Categoria</strong> <span>{{ item.categoria }}</span>
                             </div>
                         </div>
                         <div class="col-md-8 col-lg-8 Descripcion">
                             <p class="titulo">{{ item.contenido }}</p>
-                            <!--
-                            <p class="txt">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &#39;Content here, content here&#39;, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for &#39;lorem ipsum&#39; will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                            -->
-                            <!--
-                            <i class="fas fa-map-marker-alt">
-                                
-                            </i>
-                            -->
-                            <!--
-                            <strong>Lugar:</strong> <span class="txt">Universidad del Rosario, Sede, Claustro, Aula Mutis</span><br />
-                            <i class="far fa-calendar">
-                                
-                            </i>
-                            -->
                             <strong>Fecha de inicio:</strong> <span class="txt">
                             {{ dia(item.fechaInicio) }} {{ mes(item.fechaInicio) }}, de {{ year(item.fechaInicio) }}
                             </span><br />
                             <strong>Fecha de cierre:</strong> <span class="txt">
                             {{ dia(item.fechaFin) }} {{ mes(item.fechaFin) }}, de {{ year(item.fechaFin) }}
                             </span><br />
-                            <!--
-                            <i class="far fa-clock">
-                            
-                            </i>
-                    
-                            <strong>Hora:</strong> <span class="txt">De 8:00 a.m. a 12:30 p.m</span>
-                            -->
                             <div class="row">
                                 <div class="col-lg-3 Semestre_uno"><a href="">{{ item.periodo }}</a></div>
                                 <div class="col-lg-3 Cerrado"><a href="">{{ estado(item.fechaFin) }}</a></div>
@@ -465,19 +423,6 @@ Vue.component('actividades', {
                     <input id="boton-mostrar-mas" type="button" value="Ver menos" class="btn btn-primary" @click="mostrarSoloCincoMethod(true)">
                 </div>
             </div>
-            <!--
-            <div class="row justify-content-center">
-                <nav aria-label="Page navigation example">
-                    <ul class="pagination">
-                        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                    </ul>
-                </nav>
-            </div>
-            -->
         </div>
         `,
     computed: {
@@ -542,7 +487,8 @@ Vue.component('actividades', {
         },
         programasMethod(programas){
            if(programas){
-            return programas.replaceAll(";", "<br />");
+               var edicionString = programas.replaceAll(";", "</li><li>");
+               return "<li>" + edicionString + "</li>";
            }  
         },
         mostrarSoloCincoMethod(e){
@@ -612,8 +558,8 @@ const store = new Vuex.Store({
     },
     actions: {
         llamarJson: async function({ commit }){
-            //const data = await fetch('calendario-2021-prueba.json');
-            const data = await fetch('/Documentos/Calendario-academico/calendario-2021-json.json');
+            const data = await fetch('calendario-2021-prueba.json');
+            //const data = await fetch('/Documentos/Calendario-academico/calendario-2021-json.json');
             const dataJson = await data.json();
             commit('llamarJsonMutation', dataJson);
         }
